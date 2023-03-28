94bbc1a54d90773601a536bef024e6497639b7a0
Christian Manzaraz <magnusmanz@gmail.com>
2023-03-27 21:00:05 -0300

    C18S inicio

--- semana-6/js/clase-18.js
+++ semana-6/js/clase-18.js
@@ -3,14 +3,6 @@
 /* -------------------------------------------------------------------------- */
 window.addEventListener('load', function () {
 
-    const formulario = this.document.querySelector('form')
-
-    formulario.addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        postearComentario();
-    })
-
 
 })
 
@@ -18,17 +10,9 @@ window.addEventListener('load', function () {
 /*                 [2] FUNCION: capturamos los datos del form                 */
 /* -------------------------------------------------------------------------- */
 function capturarDatos() {
-    const titulo = document.querySelector('#titulo');
-    const comentario = document.querySelector('#comentario');
 
-    // armamos el objeto basado en lo que nos pide la api
-    let objeto = {
-        title: titulo.value,
-        body: comentario.value,
-        userId: 1,
-      }
+   
 
-    return objeto;
 }
 
 /* -------------------------------------------------------------------------- */
@@ -39,24 +23,10 @@ function capturarDatos() {
 
 function postearComentario() {
     // 👇 usamos nuestra funcion para capturar los datos y guardarlos como objeto
-    const datos = capturarDatos();
+    
+     
 
-    // 👇 armamos las configuraciones
-    // la api acepta JSON, por eso stringuificamos los datos
-    const configuraciones = {
-        method: 'POST',
-        body: JSON.stringify(datos),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    }
 
-    fetch('https://jsonplaceholder.typicode.com/posts', configuraciones)
-        .then((respuesta) => respuesta.json())
-        .then((data) => {
-            console.log(data);
-            renderizarRespuesta(data);
-        });
 }
 
 /* -------------------------------------------------------------------------- */
@@ -64,19 +34,7 @@ function postearComentario() {
 /* -------------------------------------------------------------------------- */
 
 function renderizarRespuesta(datos) {
-    const div = document.querySelector('.respuesta')
-    
-    const template = `
-        <p>✅ Datos cargados en el servidor</p>
-        <p>
-            Title: ${datos.title}
-        </p>
-        <p>
-            Body: ${datos.body}
-        </p>
-    `;
 
-    div.innerHTML = template;
 
 }
 
@@ -84,7 +42,10 @@ function renderizarRespuesta(datos) {
 /* -------------------------------------------------------------------------- */
 /*                               MESA DE TRABAJO                              */
 /* -------------------------------------------------------------------------- */
-// En este caso vamos a trabajar la conexion con APIS,
-// en el siguiente DOC vamos a poder ver nuestra tarea
-// 👇
-// https://docs.google.com/document/d/1ZiCPf7IICvtp6rwfxoq5Wh5dJUROKqNw/preview
+// Mesa de Trabajo Clase 18
+// To-Do API
+// Valiéndose de la documentación y el trabajo en clase, deberán lograr crear un usuario en la API y obtener su token.
+// Este proceso puede realizarse a través de la herramienta Postman, o si ya lo prefieren desde un script.
+// Documentación: https://todo-api.ctd.academy/#/tasks/createTask
+// url: https://todo-api.ctd.academy/v1/
+// Path para crear usuario users
